d3c7d6b58efabd8f7505c85d49bd5968bb2d07bf
INEFABLE-7 <1460446@senati.pe>
2023-09-07 12:28:08 -0500

    iniciamos eh isimos el c# en diseño

---
+++ LOGINC7-JEAN/Form1.cs
@@ -0,0 +1,43 @@
+namespace LOGINC7_JEAN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btningresar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btningresar_Click_1(object sender, EventArgs e)
+        {
+            Dim usuario, password As String
+
+            usuario.txt
+
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
